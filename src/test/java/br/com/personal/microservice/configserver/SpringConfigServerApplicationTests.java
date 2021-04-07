30b1ee44dbd582245bf90dcbb2f186ee819dc5c1
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-04-06 21:30:57 -0300

    Creating a Config Server project

---
+++ src/test/java/br/com/personal/microservice/configserver/SpringConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.personal.microservice.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
